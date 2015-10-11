705f503a391898ac8299b5c59250973eb8deb508
JackLam <linjue@wilead.com>
2015-10-11 23:40:32 +0800

    增加密码格式验证以及部分注释

--- lib/Util/Validator.php
+++ lib/Util/Validator.php
@@ -15,7 +15,7 @@ namespace Util;
 class Validator
 {
     /**
-     * 验证手机号码
+     * 手机号码验证
      *
      * @param string $mobile
      *
@@ -26,8 +26,36 @@ class Validator
         return preg_match('/^1[3458][0-9]{9}$/', $mobile) > 0;
     }
 
+    /**
+     * 英文字符串验证
+     *
+     * @param $string
+     *
+     * @return bool
+     */
     static public function validEnglishString($string)
     {
         return preg_match("/^[A-Za-z]+$/", $string) > 0;
+    }
+
+    /**
+     * 密码格式验证
+     *
+     * @param string $password
+     * @param int $minLength
+     * @param int $maxLength
+     *
+     * @return bool
+     */
+    static public function validPassword($password, $minLength = 6, $maxLength = 18)
+    {
+        if (!preg_match('/^(?=.*\d)(?=.*[A-Za-z])[0-9A-Za-z!@#$%]{8,12}$/', $password)) {
+            return false;
+        }
+        $length = strlen($password);
+        if ($length < $minLength || $length > $maxLength) {
+            return false;
+        }
+        return true;
     }
 }
