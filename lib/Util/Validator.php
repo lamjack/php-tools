7ed369d681efeee2c087fed824d6127d09f09cec
JackLam <linjue@wilead.com>
2015-10-11 17:56:57 +0800

    php验证类

---
+++ lib/Util/Validator.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Author: jack<linjue@wilead.com>
+ * Date: 15/10/11
+ */
+
+namespace Util;
+
+/**
+ * 常用验证类
+ *
+ * Class Validator
+ * @package Util
+ */
+class Validator
+{
+    /**
+     * 验证手机号码
+     *
+     * @param string $mobile
+     *
+     * @return boolean
+     */
+    static public function validMobileNumber($mobile)
+    {
+        return preg_match('/^1[3458][0-9]{9}$/', $mobile) > 0;
+    }
+}
