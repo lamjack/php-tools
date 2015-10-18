66c99443c2c1ff59191a1f99dc242942b4ecc854
marquis <Marquis@Marquis-MacBook-Pro.local>
2015-10-18 12:01:14 +0800

    增加邮箱地址格式验证

--- lib/Util/Validator.php
+++ lib/Util/Validator.php
@@ -58,4 +58,45 @@ class Validator
         }
         return true;
     }
+
+    /**
+     * 邮箱地址格式验证
+     *
+     * @param $email
+     * @return bool
+     */
+    static public function validEmail($email)
+    {
+        $isValid = true;
+        $atIndex = strrpos($email, "@");
+        if (is_bool($atIndex) && !$atIndex) {
+            $isValid = false;
+        } else {
+            $domain = substr($email, $atIndex + 1);
+            $local = substr($email, 0, $atIndex);
+            $localLen = strlen($local);
+            $domainLen = strlen($domain);
+            if ($localLen < 1 || $localLen > 64) {
+                $isValid = false;
+            } else if ($domainLen < 1 || $domainLen > 255) {
+                $isValid = false;
+            } else if ($local[0] == '.' || $local[$localLen - 1] == '.') {
+                $isValid = false;
+            } else if (preg_match('/\\.\\./', $local)) {
+                $isValid = false;
+            } else if (!preg_match('/^[A-Za-z0-9\\-\\.]+$/', $domain)) {
+                $isValid = false;
+            } else if (preg_match('/\\.\\./', $domain)) {
+                $isValid = false;
+            } else if (!preg_match('/^(\\\\.|[A-Za-z0-9!#%&`_=\\/$\'*+?^{}|~.-])+$/', str_replace("\\\\", "", $local))) {
+                if (!preg_match('/^"(\\\\"|[^"])+"$/', str_replace("\\\\", "", $local))) {
+                    $isValid = false;
+                }
+            }
+            if ($isValid && !(checkdnsrr($domain, "MX") || checkdnsrr($domain, "A"))) {
+                $isValid = false;
+            }
+        }
+        return $isValid;
+    }
 }
