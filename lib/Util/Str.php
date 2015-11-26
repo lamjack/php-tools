4dc94e05aae8b9b64042b7f9831b97dfcba24b0e
JackLam <jack@wizmacau.com>
2015-11-26 15:17:45 +0800

    no message

--- lib/Util/Str.php
+++ lib/Util/Str.php
@@ -65,6 +65,43 @@ class Str
     }
 
     /**
+     * 检查文本是否utf8编码
+     *
+     * @param $string
+     *
+     * @return bool
+     */
+    static public function isUTF8($string)
+    {
+        for ($idx = 0, $strlen = strlen($string); $idx < $strlen; $idx++) {
+            $byte = ord($string[$idx]);
+
+            if ($byte & 0x80) {
+                if (($byte & 0xE0) == 0xC0) {
+                    $bytes_remaining = 1;
+                } else if (($byte & 0xF0) == 0xE0) {
+                    $bytes_remaining = 2;
+                } else if (($byte & 0xF8) == 0xF0) {
+                    $bytes_remaining = 3;
+                } else {
+                    return false;
+                }
+
+                if ($idx + $bytes_remaining >= $strlen) {
+                    return false;
+                }
+
+                while ($bytes_remaining--) {
+                    if ((ord($string[++$idx]) & 0xC0) != 0x80) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    /**
      * 获取随机密码
      *
      * @param int $length
